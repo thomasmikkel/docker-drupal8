542cb32cdbcbe788fbebb6a03fbf77d814195fee
Thomas Mikkel Jensen <thomas2635@gmail.com>
2018-02-19 21:52:52 +0100

    Small fixes to settings, composer.json and .gitignore files.

--- codebase/scripts/composer/ScriptHandler.php
+++ codebase/scripts/composer/ScriptHandler.php
@@ -15,17 +15,20 @@ use Webmozart\PathUtil\Path;
 
 class ScriptHandler {
 
-  public static function createRequiredFiles(Event $event) {
+  public static function copyRequiredFiles(Event $event) {
     $fs = new Filesystem();
     $drupalFinder = new DrupalFinder();
     $drupalFinder->locateRoot(getcwd());
     $drupalRoot = $drupalFinder->getDrupalRoot();
+    $composer_root = $drupalFinder->getComposerRoot();
 
     $dirs = [
       'modules',
       'profiles',
       'themes',
     ];
+
+    $event->getIO()->write($drupalRoot);
 
     // Required for unit testing
     foreach ($dirs as $dir) {
@@ -35,30 +38,89 @@ class ScriptHandler {
       }
     }
 
-    // Prepare the settings file for installation
-    if (!$fs->exists($drupalRoot . '/sites/default/settings.php') and $fs->exists($drupalRoot . '/sites/default/default.settings.php')) {
-      $fs->copy($drupalRoot . '/sites/default/default.settings.php', $drupalRoot . '/sites/default/settings.php');
-      require_once $drupalRoot . '/core/includes/bootstrap.inc';
-      require_once $drupalRoot . '/core/includes/install.inc';
-      $settings['config_directories'] = [
-        CONFIG_SYNC_DIRECTORY => (object) [
-          'value' => Path::makeRelative($drupalFinder->getComposerRoot() . '/config/sync', $drupalRoot),
-          'required' => TRUE,
-        ],
-      ];
-      drupal_rewrite_settings($settings, $drupalRoot . '/sites/default/settings.php');
-      $fs->chmod($drupalRoot . '/sites/default/settings.php', 0666);
-      $event->getIO()->write("Create a sites/default/settings.php file with chmod 0666");
+    $files = [
+      'settings.php',
+      'settings.local.php',
+      // 'settings.ng.php',
+      'services.yml',
+      'development.services.yml',
+    ];
+
+    foreach ($files as $file) {
+      // Prepare the settings file for installation
+      if ($fs->exists($composer_root . '/settings/' . $file)) {
+        $fs->copy($composer_root . '/settings/' . $file, $drupalRoot . '/sites/default/' . $file);
+        $fs->chmod($drupalRoot . '/sites/default/' . $file, 0555);
+        $event->getIO()->write("Create a sites/default/$file file with chmod 0555");
+
+      }
     }
 
-    // Create the files directory with chmod 0777
-    if (!$fs->exists($drupalRoot . '/sites/default/files')) {
-      $oldmask = umask(0);
-      $fs->mkdir($drupalRoot . '/sites/default/files', 0777);
-      umask($oldmask);
-      $event->getIO()->write("Create a sites/default/files directory with chmod 0777");
+    $file_directories = [
+      'files',
+      'private',
+      'tmp',
+    ];
+
+    $directory_path = '/sites/default/';
+
+    foreach ($file_directories as $file) {
+      // Create the files directory with chmod 0777
+      if (!$fs->exists($drupalRoot . $directory_path . $file)) {
+        $oldmask = umask(0);
+        $fs->mkdir($drupalRoot . $directory_path . $file, 0777);
+        umask($oldmask);
+      }
+      $fs->chown($drupalRoot . $directory_path . $file, 'www-data', TRUE);
+      $event->getIO()->write("Create a $file directory with chmod 0777 and change ownership to www-data.");
     }
+
   }
+
+  // public static function createRequiredFiles(Event $event) {
+  //   $fs = new Filesystem();
+  //   $drupalFinder = new DrupalFinder();
+  //   $drupalFinder->locateRoot(getcwd());
+  //   $drupalRoot = $drupalFinder->getDrupalRoot();
+  //
+  //   $dirs = [
+  //     'modules',
+  //     'profiles',
+  //     'themes',
+  //   ];
+  //
+  //   // Required for unit testing
+  //   foreach ($dirs as $dir) {
+  //     if (!$fs->exists($drupalRoot . '/'. $dir)) {
+  //       $fs->mkdir($drupalRoot . '/'. $dir);
+  //       $fs->touch($drupalRoot . '/'. $dir . '/.gitkeep');
+  //     }
+  //   }
+  //
+  //   // Prepare the settings file for installation
+  //   if (!$fs->exists($drupalRoot . '/sites/default/settings.php') and $fs->exists($drupalRoot . '/sites/default/default.settings.php')) {
+  //     $fs->copy($drupalRoot . '/sites/default/default.settings.php', $drupalRoot . '/sites/default/settings.php');
+  //     require_once $drupalRoot . '/core/includes/bootstrap.inc';
+  //     require_once $drupalRoot . '/core/includes/install.inc';
+  //     $settings['config_directories'] = [
+  //       CONFIG_SYNC_DIRECTORY => (object) [
+  //         'value' => Path::makeRelative($drupalFinder->getComposerRoot() . '/config/sync', $drupalRoot),
+  //         'required' => TRUE,
+  //       ],
+  //     ];
+  //     drupal_rewrite_settings($settings, $drupalRoot . '/sites/default/settings.php');
+  //     $fs->chmod($drupalRoot . '/sites/default/settings.php', 0666);
+  //     $event->getIO()->write("Create a sites/default/settings.php file with chmod 0666");
+  //   }
+  //
+  //   // Create the files directory with chmod 0777
+  //   if (!$fs->exists($drupalRoot . '/sites/default/files')) {
+  //     $oldmask = umask(0);
+  //     $fs->mkdir($drupalRoot . '/sites/default/files', 0777);
+  //     umask($oldmask);
+  //     $event->getIO()->write("Create a sites/default/files directory with chmod 0777");
+  //   }
+  // }
 
   /**
    * Checks if the installed version of Composer is compatible.
